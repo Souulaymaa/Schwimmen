554d1008ec978543ad2315aff2e73eedaf7a8d99
Salma Dhimene <salma.dhimene@tu-dortmund.de>
2022-05-04 15:43:37 +0200

    Replace Player.kt

--- src/main/kotlin/entity/Player.kt
+++ src/main/kotlin/entity/Player.kt
@@ -2,7 +2,23 @@ package entity
 
 import java.util.ArrayDeque
 
+/**
+ * Die Entität [Player].
+ *
+ * Der Konstruktor erwartet einen Namen, die Handkarten und die Variable [knocked], die uns kennenzeichnet, dass der Spieler
+ * geklopft hat.
+ *
+ * @param playerName Der Spielername.
+ * @param playerCards Die Handkarten.
+ * @throws IllegalArgumentException Wenn der Spielername leer ist
+ */
+
 class Player( val playerName: String, val playerCards : ArrayDeque<Card> = ArrayDeque(3)) {
     override fun toString(): String = "$playerName"
-    val knocked = false
+    val knocked: Boolean = false
+    init{
+        if(playerName == ""){
+            throw IllegalArgumentException("The Player name is empty")
+        }
+    }
 }
