948454ab0e13cb3f9368de2c2749e7dfcd905fc2
Salma Dhimene <salma.dhimene@tu-dortmund.de>
2022-05-04 16:54:33 +0200

    Replace CardStack.kt

--- src/main/kotlin/entity/CardStack.kt
+++ src/main/kotlin/entity/CardStack.kt
@@ -1,38 +1,16 @@
 package entity
 import java.util.*
 
-class CardStack (private val drawStack: ArrayDeque<Card> = ArrayDeque(32), val tableStack : ArrayDeque<Card> = ArrayDeque(3)) {
-    // check if drawStack still has cards
-    val empty: Boolean get() = drawStack.isEmpty()
 
-    // check how many cards there are on the stack
-    val size: Int get() = drawStack.size
+class CardStack ( val drawStack: ArrayList<Card> , var tableStack : ArrayList<Card>) {
 
-    //returning the top card from the stack
-    fun peek() : Card = drawStack.first()
-
-    fun putOnTop(cards: List<Card>) {
-        cards.forEach(this.drawStack::addFirst)
+    init{
+        if(tableStack.size != 3){
+            throw IllegalArgumentException("The number of table cards does not equal 3")
+        }
+        if(drawStack.size > 32 || drawStack.size < 1){
+            throw IllegalArgumentException("Invalid number of cards on the stack")
+        }
     }
-
-    fun putOnTop(card: Card) {
-        drawStack.addFirst(card)
-    }
-
-    /**
-     * Draws [amount] cards from the draw stack.
-     *
-     * @param amount the number of cards to draw; defaults to 1 if omitted.
-     *
-     * @throws IllegalArgumentException if not enough cards on stack to draw the desired amount.
-     */
-    fun draw(amount: Int = 1): List<Card> {
-        require (amount in 1..drawStack.size) { "can't draw $amount cards from $drawStack" }
-        return List(amount) { drawStack.removeFirst() }
-    }
-
-    fun drawAll(): List<Card> = draw(size)
-
-    override fun toString(): String = drawStack.toString()
 
 }
