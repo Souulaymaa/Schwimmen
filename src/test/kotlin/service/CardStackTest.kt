569b2ce4e94f0d1c741f7070248fade4ae5fc96d
Salma Dhimene <salma.dhimene@tu-dortmund.de>
2022-05-04 16:55:44 +0200

    Replace CardStackTest.kt

--- src/test/kotlin/service/CardStackTest.kt
+++ src/test/kotlin/service/CardStackTest.kt
@@ -5,39 +5,40 @@ import entity.Card
 import entity.CardSuit
 import entity.CardValue
 import entity.CardStack
+import java.lang.IllegalArgumentException
 
 /**
  * Test cases for [CardStack]
  */
 class CardStackTest {
-    private val aceOfSpades = Card(CardSuit.SPADES, CardValue.ACE)
-    private val jackOfClubs = Card(CardSuit.CLUBS, CardValue.JACK)
-    private val queenOfHearts = Card(CardSuit.HEARTS, CardValue.QUEEN)
+    //initialise some table cards and drawstacks to test with
+    private var tablecards1 = arrayListOf<Card>(
+        Card(CardSuit.HEARTS, CardValue.QUEEN),
+        Card(CardSuit.CLUBS, CardValue.SEVEN),
+        Card(CardSuit.SPADES, CardValue.JACK))
+    private var tablecards2 = arrayListOf<Card>(
+        Card(CardSuit.HEARTS, CardValue.QUEEN),
+        Card(CardSuit.CLUBS, CardValue.SEVEN))
+    private val drawcards1 = arrayListOf<Card>(
+        Card(CardSuit.HEARTS, CardValue.QUEEN),
+        Card(CardSuit.CLUBS, CardValue.SEVEN),
+        Card(CardSuit.SPADES, CardValue.JACK),
+        Card(CardSuit.DIAMONDS, CardValue.KING))
+    private val drawcards2 = arrayListOf<Card>()
 
+
+    //test with a wrong number of table cards
     @Test
-    fun testOrder() {
-
-        val stack = CardStack()
-
-        stack.putOnTop(listOf(aceOfSpades, jackOfClubs, queenOfHearts))
-
-        assertSame(stack.peek(), queenOfHearts)
-        assertSame(stack.draw().first(), queenOfHearts)
-        assertSame(stack.peek(), jackOfClubs)
-        assertEquals(stack.size, 2)
-
-        stack.putOnTop(queenOfHearts)
-        assertSame(stack.peek(), queenOfHearts)
-        assertEquals(stack.size, 3)
-
+    fun wrongTableCardsNumber(){
+        assertFailsWith<IllegalArgumentException>(){
+            CardStack(drawcards1, tablecards2)
+        }
     }
-
+    //test with a wrong number of draw cards
     @Test
-    fun testDrawFail() {
-        val stack = CardStack()
-        assertFails { stack.draw() }
-        stack.putOnTop(listOf(aceOfSpades, jackOfClubs, queenOfHearts))
-        stack.drawAll()
-        assertFails { stack.draw() }
+    fun wrongDrawCardsNumber(){
+        assertFailsWith<IllegalArgumentException>(){
+            CardStack(drawcards2, tablecards1)
+        }
     }
 }
