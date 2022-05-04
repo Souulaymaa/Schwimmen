c5782974412bc4dca37ab2fb65171be250682854
Salma Dhimene <salma.dhimene@tu-dortmund.de>
2022-05-04 15:34:39 +0200

    Replace Card.kt

--- src/main/kotlin/entity/Card.kt
+++ src/main/kotlin/entity/Card.kt
@@ -1,6 +1,17 @@
 package entity
 
+/**
+ * Die Klasse für die einzelne Entität die der Spiel kennt.
+ *
+ * Der Konstruktor erwartet ein Symbol [cardSuit] und einen Wert [cardValue].
+ *
+ * @param cardSuit Das Suit der Karte.
+ * @param cardValue Der Wert der Karte.
+ */
+
 class Card(val suit: CardSuit, val value: CardValue) {
+
     override fun toString() = "$suit$value"
+
     operator fun compareTo(other: Card) = this.value.ordinal - other.value.ordinal
 }
