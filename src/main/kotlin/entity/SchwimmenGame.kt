6da7b4215060fd85823bf80cd18d27beab57314b
Salma Dhimene <salma.dhimene@tu-dortmund.de>
2022-05-04 16:54:10 +0200

    Replace SchwimmenGame.kt

--- src/main/kotlin/entity/SchwimmenGame.kt
+++ src/main/kotlin/entity/SchwimmenGame.kt
@@ -1,5 +1,18 @@
 package entity
 import java.util.*
 
-class SchwimmenGame(val players : Queue<Player> ,val passCount : Int )
+/**
+ * Eine Entitätsklasse, die der Spiel darstellt.
+ *
+ * @param [players] Die Liste der Spieler
+ * @throws IllegalArgumentException wenn die Anzahl der Spieler ungültig ist
+ */
+class SchwimmenGame(val players : Queue<Player>  ){
+    var passCount: Int = 0
+    init {
+        if(players.count() < 2 || 4 < players.count()){
+            throw IllegalArgumentException("The number of Players should be 2-4")
+        }
+    }
+}
 
